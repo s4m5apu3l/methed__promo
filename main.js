b5d3fb572717b70e708e7b4c3f712cb689f6bec1
s4m5apu3l <asperatuz@yandex.ru>
2022-02-19 22:55:44 +0900

    feat: add adaptive and burger menu

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('true');
+const burger = document.querySelector('.menu-burger');
+const menu = document.querySelector('.header__nav');
+let menuOpen = false;
+
+burger.addEventListener('click', () => {
+  if(!menuOpen) {
+    burger.classList.add('is-active');
+    menuOpen = true;
+    menu.classList.add('header__nav-active');
+  } else {
+    burger.classList.remove('is-active');
+    menuOpen = false;
+    menu.classList.remove('header__nav-active');
+  }
+})
+
+function closeMenu() {
+  menu.classList.remove('header__nav-active');
+  burger.classList.remove('is-active');
+}
